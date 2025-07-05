ae6467a74ce91f5c4423ef4fbc7db0b966ba3cc8
print-aash <ashusingh88106283@gmail.com>
2025-07-05 22:29:56 +0530

    Add files via upload

---
+++ lovegem.py/lovegem.py
@@ -0,0 +1,19 @@
+import random
+
+def love_gem():
+    user_first_input = input("Enter First Name ")
+    user_Second_input = input("Enter second Name ")
+
+    percentage = random.randint(0 , 100 )
+
+    print("love compatibiThelity between " , {user_first_input} , "and" , {user_Second_input} , "is" , percentage)
+
+love_gem()
+
+while True:
+    love_gem()
+    again = input("Try again? (yes/no): ").lower()
+    if again != "yes":
+        break
+# while True:
+    # love_gem
